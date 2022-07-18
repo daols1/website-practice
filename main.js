e0d732a3bb96b9ab29490805d7f8763bd68d645a
David <78326757+daols1@users.noreply.github.com>
2022-07-18 16:31:51 +0100

    Head done :+1:

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let load = document.getElementById('load')
 let body = document.querySelector('body')
 let mobileMenu = document.getElementById('hamburger')
+let navBar = document.querySelector('.nav-bar')
+let mobileMenuCancel = document.getElementById('cancel')
 // mobileMenu.style.display = 'none';
 body.style.visibility = 'hidden';
 load.style.visibility = 'visible';
@@ -9,37 +11,11 @@ setTimeout(() => {
     load.style.display = 'none';
     body.style.visibility = 'visible';
 }, 2000);
-function hamburger(){
-    
-}
+mobileMenu.addEventListener('click', () => {
+    navBar.style.visibility = 'visible';
+    navBar.style.animation = 'enter 5s';
+})
 
-
-
-
-
-
-
-
-
-// const logo = document.getElementById('l')
-// // load.style.display = 'none';
-
-// function loader(window, load){
-//     load.style.display = 'none';
-//     window.onload = init;
-//     function init(){
-//         setTimeout(() => {
-//             load.display = 'block';
-//         }, 9000)
-//     }
-// }
-
-// loader(window, load)
-
-// logo.addEventListener('click', () => {
-//     document.style.display = 'none';
-//     load.style.display = 'block';
-//     setTimeout(() => {
-//         document.style.display = 'block';
-//     }, 2000)
-// });
+mobileMenuCancel.addEventListener('click', () => {
+    navBar.style.visibility = 'hidden';
+})
