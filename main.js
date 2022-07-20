68139f1aa0c9b680acbb35ba9ff6eda35dea78f7
David <78326757+daols1@users.noreply.github.com>
2022-07-20 06:42:58 +0100

    working

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ let body = document.querySelector('body')
 let mobileMenu = document.getElementById('hamburger')
 let navBar = document.querySelector('.nav-bar')
 let mobileMenuCancel = document.getElementById('cancel')
+const slide1 = document.querySelector('.slide-1')
+const slide2 = document.querySelector('.slide-2')
+const slide3 = document.querySelector('.slide-2')
+const arrowLeft = document.querySelector('.arr-l')
+const arrowRight = document.querySelector('.arr-r')
 // mobileMenu.style.display = 'none';
-
 body.style.display = 'none';
 window.scrollTo(0, 0)
 load.style.visibility = 'visible';
@@ -23,3 +27,28 @@ mobileMenu.addEventListener('click', () => {
 mobileMenuCancel.addEventListener('click', () => {
     navBar.style.visibility = 'hidden';
 })
+
+// slide1.style.display = 'none';
+// slide2.style.display = 'none';
+// slide3.style.display = 'none';
+
+// let currentClick = 0 
+// scrollArr.forEach(currentClick+= scrollArr[currentClick])
+
+arrowLeft.addEventListener('click', () => {
+    scroll(currentClick)
+    slide1.style.display = 'none';
+    slide2.style.display = 'grid';
+})
+
+let currentClick = 0
+
+function scroll(currentClick){
+    let scrollArr = [slide1, slide2, slide3]
+    let clicker = scrollArr[currentClick]
+    if (clicker !== 3){
+        clicker++
+    } else{
+        console.log('wow you just did rubbish')
+    }
+}
